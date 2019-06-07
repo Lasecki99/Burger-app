ec10c202b1fec57994cc47a1fcbab4cdb9d1f0d0
Lasecki99 <marcin.lasecki99@gmail.com>
2019-06-07 12:50:27 +0200

    Updates

---
+++ precache-manifest.e3d94f07f89537372646b51e64656464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c358aa82909f3276660a76ed94544e",
+    "url": "/Burger-app/index.html"
+  },
+  {
+    "revision": "874da8557970f494ca28",
+    "url": "/Burger-app/static/css/main.610318d3.chunk.css"
+  },
+  {
+    "revision": "e6fbe87e64d12daf5103",
+    "url": "/Burger-app/static/js/2.37e8bdc3.chunk.js"
+  },
+  {
+    "revision": "874da8557970f494ca28",
+    "url": "/Burger-app/static/js/main.7e50fdb2.chunk.js"
+  },
+  {
+    "revision": "a99b80ea7385cfc635bc",
+    "url": "/Burger-app/static/js/runtime~main.ec93ba54.js"
+  }
+]);
